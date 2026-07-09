edebacbb443706090fb63708bcb2fc6eafdb1878
demirksmca-maker <demirksmca@gmail.com>
2026-07-09 14:24:27 -0400

    Fix syntax error in groq-proxy.js

--- api/groq-proxy.js
+++ api/groq-proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+ö9export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
